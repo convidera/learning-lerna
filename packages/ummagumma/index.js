ff4be265b3f41bd83e84a0a4cf494c17758662c5
Mariia Shishkina <shishkina@convidera.com>
2023-05-11 12:22:54 +0200

    feat(ummagumma): adds so many songs BREAKING CHANGE: changes one song to a whole array of them

--- packages/ummagumma/index.js
+++ packages/ummagumma/index.js
@@ -1,3 +1,15 @@
+const songs = [
+  "Astronomy Domine",
+  "Careful With That Axe, Eugene",
+  "Set the Controls for the Heart of the Sun",
+  "A Saucerful of Secrets",
+  "Sysyphus",
+  "Grantchester Meadows",
+  "Several Species of Small Furry Animals Gathered Together in a Cave and Grooving with a Pict",
+  "The Narrow Way – Parts 1, 2 & 3",
+  "The Grand Vizier's Garden Party",
+];
+
 (() => {
-  console.log('Careful with that axe, Eugine!');
+  songs.forEach((song) => console.log(song));
 })();
